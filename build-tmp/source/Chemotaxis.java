b9c687f213eb2db66f802e922d75878a2841fe66
tianlunlee <tianlunlee@gmail.com>
2015-09-17 11:34:48 -0700

    working move function

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 
 Bacteria [] colony = new Bacteria[100];
 
@@ -7,7 +23,7 @@ int i =0;
 
 
  //declare bacteria variables here   
- void setup()   
+ public void setup()   
  {     
  	//initialize bacteria variables here  
  	size(400, 400);
@@ -23,7 +39,7 @@ int i =0;
 
 
  }
- void draw()   
+ public void draw()   
  {    
  	background(101);
  	//move and show the bacteria 
@@ -52,21 +68,21 @@ int i =0;
  		bactCol = col;
  	}
 
- 	void show() {
+ 	public void show() {
  		fill(bactCol, bactX, bactY);
  		ellipse(bactX,bactY,5,5);
 
  	}
 
- 	void move(){
+ 	public void move(){
  		// x component
  		if (mouseX < bactX) 
  		{
- 			bactX += (int)(Math.random()*3) -1.5;
+ 			bactX += (int)(Math.random()*3) -1.5f;
  		} 	
 
  		else if (mouseX > bactX) {
- 			bactX += (int)(Math.random()*3) +1.5;
+ 			bactX += (int)(Math.random()*3) +1.5f;
  		} 	
 
  		else if (mouseX == bactX)
@@ -78,11 +94,11 @@ int i =0;
 
  		if ( mouseY < bactY) 
  		{
- 			bactY += (int)(Math.random()*3) - 1.5;
+ 			bactY += (int)(Math.random()*3) - 1.5f;
  		}
 
  		else if (mouseY > bactY) {
- 			bactY += (int)(Math.random()*3) +1.5;
+ 			bactY += (int)(Math.random()*3) +1.5f;
  		}
 
  		else if (mouseY == bactY)
@@ -93,3 +109,12 @@ int i =0;
 
  	}
  }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
